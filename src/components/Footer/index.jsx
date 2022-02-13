7cbb0af565b7099c7c60fcce1f56de9de18ffd9c
Abián Izquierdo <abizmo@gmail.com>
2022-02-13 21:39:05 +0000

    Initial commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p className="attribution">
+        Challenge by
+        {' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>
+        .
+        Coded by
+        {' '}
+        <a href="https://www.abizmo.dev">Abián Izquierdo</a>
+        .
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
