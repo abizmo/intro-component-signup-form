ac2e80339974a6ec685007f442910b0907ee4a91
abizmo <abizmo@gmail.com>
2022-02-14 14:55:36 +0000

    Add style to header and banner

---
+++ src/components/SignUpForm/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SignUpForm = () => (
+  <form>
+    First Name
+    Last Name
+    Email Address
+    Password
+
+    Claim your free trial
+
+    By clicking the button, you are agreeing to our Terms and Services
+  </form>
+);
+
+export default SignUpForm;
