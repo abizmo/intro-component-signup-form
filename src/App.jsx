4056ef35ff03ac5f59897da9801a51b5204f308e
abizmo <abizmo@gmail.com>
2022-02-14 12:55:01 +0000

    Add Intro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-/* eslint-disable max-len */
 import React from 'react';
+
 import Footer from './components/Footer';
+import Intro from './components/Intro';
 
 const App = () => (
   <>
-    <h1>
-      Learn to code by watching others
-    </h1>
-    <h2>
-      See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
-      but understanding how developers think is invaluable.
-    </h2>
-    <div>
-      <p>
-        <b>Try it free 7 days</b>
-        {' '}
-        then $20/mo. thereafter
-      </p>
-    </div>
-    <form>
-      First Name
-      Last Name
-      Email Address
-      Password
-
-      Claim your free trial
-
-      By clicking the button, you are agreeing to our Terms and Services
-    </form>
-
-    <Footer />
+    <Intro />
+    <Footer style={{ color: 'white' }} />
   </>
 );
 
