06ae03c60ba98a72ef01652b13f5fcb3c42cdb35
Abián Izquierdo <abizmo@gmail.com>
2022-02-16 16:03:53 +0000

    Validate email

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,61 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './SignUpForm.module.css';
 
-const SignUpForm = () => (
-  <form className={styles.signUpForm}>
-    <div className={styles.input}>
-      <input
-        autoComplete="off"
-        id="firstName"
-        name="firstName"
-        placeholder="First Name"
-        type="text"
-      />
-    </div>
-    <div className={styles.input}>
-      <input
-        autoComplete="off"
-        id="lastName"
-        name="lastName"
-        placeholder="Last Name"
-        type="text"
-      />
-    </div>
-    <div>
-      <div className={`${styles.input} ${styles.error}`}>
+const SignUpForm = () => {
+  const [email, setEmail] = useState({
+    value: '',
+    error: '',
+  });
+
+  const isEmpty = (value) => value.trim() === '';
+
+  const validateEmail = (emailToValidate) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (isEmpty(emailToValidate)) return 'Email can not be empty';
+    if (!emailToValidate.match(re)) return 'Looks like this is not an email';
+    return '';
+  };
+
+  const handleChange = ({ value }) => {
+    setEmail({
+      value,
+      error: validateEmail(value),
+    });
+  };
+
+  return (
+    <form className={styles.signUpForm}>
+      <div className={styles.input}>
         <input
           autoComplete="off"
-          id="email"
-          name="email"
-          placeholder="Email Address"
+          id="firstName"
+          name="firstName"
+          placeholder="First Name"
           type="text"
-          value="email@example/com"
         />
-        <i className="fa-solid fa-circle-exclamation" />
       </div>
-      <em className={styles.message}>Looks like this is not an email</em>
-    </div>
-    <div className={styles.input}>
-      <input
-        autoComplete="off"
-        id="password"
-        name="password"
-        placeholder="Password"
-        type="password"
-      />
-    </div>
-    <div className={styles.action}>
-      <button type="submit">
-        Claim your free trial
-      </button>
-      <p>
-        By clicking the button, you are agreeing to our
-        {' '}
-        <b>Terms and Services</b>
-      </p>
-    </div>
-  </form>
-);
+      <div className={styles.input}>
+        <input
+          autoComplete="off"
+          id="lastName"
+          name="lastName"
+          placeholder="Last Name"
+          type="text"
+        />
+      </div>
+      <div>
+        <div className={`${styles.input} ${!!email.error && styles.error}`}>
+          <input
+            autoComplete="off"
+            id="email"
+            name="email"
+            onChange={({ target }) => handleChange(target)}
+            placeholder="Email Address"
+            type="text"
+            value={email.value}
+          />
+          {
+            !!email.error
+            && <i className="fa-solid fa-circle-exclamation" />
+          }
+        </div>
+        {
+          !!email.error
+          && <em className={styles.message}>{email.error}</em>
+        }
+      </div>
+      <div className={styles.input}>
+        <input
+          autoComplete="off"
+          id="password"
+          name="password"
+          placeholder="Password"
+          type="password"
+        />
+      </div>
+      <div className={styles.action}>
+        <button type="submit">
+          Claim your free trial
+        </button>
+        <p>
+          By clicking the button, you are agreeing to our
+          {' '}
+          <b>Terms and Services</b>
+        </p>
+      </div>
+    </form>
+  );
+};
 
 export default SignUpForm;
